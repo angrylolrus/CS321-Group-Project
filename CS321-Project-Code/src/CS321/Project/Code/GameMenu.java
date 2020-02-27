fc464adc18524f374eddb95b095c989bc5176edc
Sneh Talwar <izinfany@gmail.com>
2020-02-26 21:00:57 -0500

    working on gamemenu class

--- CS321-Project-Code/src/CS321/Project/Code/GameMenu.java
+++ CS321-Project-Code/src/CS321/Project/Code/GameMenu.java
@@ -1,7 +1,27 @@
 package CS321.Project.Code;
 
+import java.awt.*;
+import java.awt.event.*;
+
 public class GameMenu {
 	Controller controller;
 	Player player;
-	World world;
+	// World world;
+
+
+	public GameMenu(Controller controller) {
+
+	}
+
+	public void receiveMouse(MouseEvent e, int num) {
+
+	}
+
+	public void receiveKey(KeyEvent e, int num) {
+
+	}
+
+	public void draw(Graphics g) {
+
+	}
 }
