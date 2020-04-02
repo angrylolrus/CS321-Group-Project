4a680e25900279522d0ee775526f2df72be36d77
Jarrod Q <lolrus1995@hotmail.com>
2020-04-02 16:26:58 -0400

    :)

--- CS321-Project-Code/src/CS321/Project/Code/GameMenu.java
+++ CS321-Project-Code/src/CS321/Project/Code/GameMenu.java
@@ -2,12 +2,14 @@ package CS321.Project.Code;
 
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.awt.event.KeyEvent;
 
 public class GameMenu {
 	Controller controller;
 	Player player;
 	World world;
+	public ArrayList<UIElement> elements;
 	
 	public GameMenu(Controller c) {
 		controller = c;
